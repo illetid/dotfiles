ac473a0305a4b597b16f300c00c2c5eb9616f32d
Andrii Ovcharenko <illetid@gmail.com>
2024-02-22 13:34:05 +0100

    change macos settings

--- macos_custom_settings.sh
+++ macos_custom_settings.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Disable font smoothing
-defaults -currentHost write -g AppleFontSmoothing -int 0
 
+# Disable font smoothing
+defaults -currentHost write -g AppleFontSmoothing -int 0;
+
+defaults write com.apple.dock "autohide" -bool "true";
 # Simple window scale effect
 defaults write com.apple.dock mineffect -string scale;
 
@@ -12,6 +14,8 @@ defaults write com.apple.dock autohide-delay -float 0;
 # Fast Dock showing animation (300ms)
 defaults write com.apple.dock autohide-time-modifier -float 0.3;
 
+# Show pathbar
+defaults write com.apple.finder "ShowPathbar" -bool "true";
 # Set default folder for screenshots
 mkdir ~/Screenshots;
 defaults write com.apple.screencapture location ~/Screenshots;
@@ -19,3 +23,4 @@ defaults write com.apple.screencapture location ~/Screenshots;
 # Restart services
 killall Dock;
 killall SystemUIServer;
+killall Finder;
